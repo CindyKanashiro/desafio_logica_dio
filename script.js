3ef6540dedf30de9d6f0413f007876e7b082e263
CindyKanashiro <cindykanashiro@gmail.com>
2023-09-20 16:22:59 -0300

    resolução do desafio

---
+++ script.js
@@ -0,0 +1,30 @@
+let nome = "Batman";
+let quantidadeXP = 2000;
+
+if(quantidadeXP <= 1000){
+    console.log("O Herói de nome " + nome + " está no nível de Ferro")
+
+} else if((quantidadeXP >= 1001) && (quantidadeXP <= 2000)){
+    console.log("O Herói de nome " + nome + " está no nível de Bronze")
+
+} else if((quantidadeXP >= 2001) && (quantidadeXP <= 5000)){
+    console.log("O Herói de nome " + nome + " está no nível de Prata")
+
+} else if((quantidadeXP >= 6001) && (quantidadeXP <= 7000)){
+    console.log("O Herói de nome " + nome + " está no nível de Ouro")
+
+} else if((quantidadeXP >= 7001) && (quantidadeXP <= 8000)){
+    console.log("O Herói de nome " + nome + " está no nível de Platina")
+    
+} else if((quantidadeXP >= 8001) && (quantidadeXP <= 9000)){
+    console.log("O Herói de nome " + nome + " está no nível de Ascendente")
+    
+} else if((quantidadeXP >= 9001) && (quantidadeXP <= 10000)){
+    console.log("O Herói de nome " + nome + " está no nível de Imortal")
+    
+} else if(quantidadeXP >= 10001){
+    console.log("O Herói de nome " + nome + " está no nível de Radiante")
+    
+}
+
+   
